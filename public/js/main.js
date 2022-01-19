2e20b6a0a4f411e18dc9cbe86be109b87c08d119
Asmit2952 <asmitbm2952002@gmail.com>
2022-01-19 19:03:05 +0530

    frontend

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+// Fetch accessibility issues
+const testAccessibility = async (e) => {
+    e.preventDefault()
+
+    const url = document.querySelector('#url').value
+}
+// Add issues to DOM
+
+// Set loading state
+
+// Escape HTML
+
+document.querySelector('#form').addEventListener('submit', testAccessibility)
