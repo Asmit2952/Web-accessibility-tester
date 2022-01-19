beeb4a483033d24f90c79e19be14f8df183b088d
Asmit2952 <asmitbm2952002@gmail.com>
2022-01-19 19:14:32 +0530

    fetch accessibility issues

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,39 @@ const testAccessibility = async (e) => {
     e.preventDefault()
 
     const url = document.querySelector('#url').value
+
+    if(url === '') {
+        alert('Please add a URL')
+    }
+    else {
+        setLoading()
+
+        const response = await fetch(`/api/test?url=${url}`)
+
+        if(response.status !== 200) {
+            setLoading(false)
+            alert('Something went wrong :(')
+        }
+        else {
+            const {issues} = await response.json()
+            addIssuesToDOM(issues)
+            setLoading(false)
+        }
+    }
 }
 // Add issues to DOM
 
 // Set loading state
+const setLoading = (isLoading = true) => {
+    const loader = document.querySelector('.loader')
+    if(isLoading) {
+        loader.style.display = 'block'
+    }
+    else {
+        loader.style.display = 'none'
+    }
+}
+
 
 // Escape HTML
 
